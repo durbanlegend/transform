a21d2aef4b619db291c08057a40f3a43ef336941
durbanlegend <dforbes@iafrica.com>
2014-08-30 08:45:27 +0200

    First baby steps at a transformation.

--- ReadFile.go
+++ ReadFile.go
@@ -15,19 +15,39 @@ func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println("Value of i is now:", i)
 	}
+
+	// open output file
+	fo, err := os.Create("/tmp/output.txt")
+	if err != nil {
+		panic(err)
+	}
+	// close fo on exit and check for its returned error
+	defer func() {
+		if err := fo.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// make a write buffer
+	w := bufio.NewWriter(fo)
+
 	start := time.Now()
 	i := 0
 	for scanner.Scan() {
 		i++
 		// fmt.Println(scanner.Text()) // Println will add back the final '\n'
-		fmt.Printf("%q\n", strings.Split(scanner.Text(), ","))
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		// fmt.Printf("%q\n", strings.Split(scanner.Text(), ","))
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading input:", err)
+		}
+		_, err := w.WriteString(strings.Join(strings.Split(scanner.Text(), ","), ";") + "\n")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "writing output:", err)
+		}
 	}
 	end := time.Now()
 	delta := end.Sub(start)
-	// fmt.Printf("Full read took this amount of time: %s\n”, delta)
+	//fmt.Println("Full transform took”, delta)
 	fmt.Println("Delta is", delta)
 	fmt.Println("Value of i is now:", i)
 }
